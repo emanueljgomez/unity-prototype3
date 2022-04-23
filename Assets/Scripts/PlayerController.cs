c0ce0a004ef5430a78e81e49d44c764ce6c25926
Emanuel J. Gomez Sanchez <emanueljgomez@gmail.com>
2022-04-23 19:58:51 -0300

    Adds sound and particle effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,25 @@ public class PlayerController : MonoBehaviour
     // Variables
     private Rigidbody playerRb;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpforce;
     public float gravityModifier;
     public bool isOnGround;
     public bool gameOver = false;
+
+    public AudioClip crashSound;
+
+    public AudioClip jumpSound;
+
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -27,8 +36,10 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             isOnGround = false;
-            playerAnim.SetTrigger("Jump_trigger");
+            playerAnim.SetTrigger("Jump_trig");
             // Inside Unity: Animator (Controller) > Layer > Jumping --> Click the arrow that goes from 'Run' to 'Running_Jump', then under 'Conditions' check the name of the trigger
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -38,6 +49,7 @@ public class PlayerController : MonoBehaviour
         // Checks for a collision event and then checks if the collided object has a specific tag name
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
@@ -45,6 +57,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true); // Animation condition: 'Death_b' parameter for death animation must be true
             playerAnim.SetInteger("DeathType_int", 1); // Animation condition: set the Condition of death animation to number 1 (the animation is called Death_01)
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
